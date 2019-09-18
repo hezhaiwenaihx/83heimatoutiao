a86decf64d423de492b64f15bac6e40b84ee8f06
hezhaiwenaihx <786672798@qq.com>
2019-09-18 19:54:17 +0800

    dsa

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+// 映入element插件
+import 'element-ui/lib/theme-chalk/index.css'
+// 映入样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+// 全局注册element
 
 new Vue({
   router,
